c07c1dfb02fcc151000fc2371d5d4923c8f7e185
lvcq <1594850919@qq.com>
2023-10-08 10:59:48 +0800

    Eureka基础

---
+++ eureka-client-A-1/src/main/java/com/lv/EurekaClientA1Application.java
@@ -0,0 +1,15 @@
+package com.lv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class EurekaClientA1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientA1Application.class, args);
+    }
+
+}
